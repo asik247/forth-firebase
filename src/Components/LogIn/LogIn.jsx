293b3aba2c59a2910d4822786c59166480cb98d2
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-19 15:01:18 +0600

    Registation and login design

--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -3,7 +3,28 @@ import React from 'react';
 const LogIn = () => {
     return (
         <div>
-            <h2>LogIn</h2>
+            <div className="hero  min-h-screen">
+                <div className="hero-content flex-col ">
+                    <div className="text-center">
+                        <h1 className="text-5xl font-bold mb-5">Login now!</h1>
+
+                    </div>
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <div className="card-body">
+                            <form>
+                                <fieldset className="fieldset">
+                                    <label className="label">Email</label>
+                                    <input type="email" className="input" placeholder="Email" />
+                                    <label className="label">Password</label>
+                                    <input type="password" className="input" placeholder="Password" />
+                                    <div><a className="link link-hover">Forgot password?</a></div>
+                                    <button className="btn btn-neutral mt-4">Login</button>
+                                </fieldset>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Registation/Registation.jsx
+++ src/Components/Registation/Registation.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Registation = () => {
+    // handle submit;
+    const hanldeSubmit = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value
+        console.log(email, password);
+    }
     return (
         <div>
-            <h1>Registation</h1>
+            <div className="hero min-h-screen">
+                <div className="hero-content flex-col ">
+                    <div className="text-center ft">
+                        <h1 className="text-5xl font-bold mb-5">Registation now!</h1>
+
+                    </div>
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <div className="card-body">
+                            <form onSubmit={hanldeSubmit}>
+                                <fieldset className="fieldset">
+                                    {/* Email */}
+                                    <label className="label">Email</label>
+                                    <input type="email" name='email' className="input" placeholder="Email" />
+                                    {/* Password */}
+                                    <label className="label">Password</label>
+                                    <input type="password" name='password' className="input" placeholder="Password" />
+
+                                    <button className="btn btn-neutral mt-4">Registation</button>
+                                </fieldset>
+                                {/* link code here */}
+                                <li className='text-sm list-none'>Already have'n account ? please <Link to={'/login'}  className='text-blue-600 font-bold text-sm underline'>LogIn</Link></li>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
