67e7ca38426a9ea822a17ee57fdee84c273872b8
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-20 09:25:56 +0600

    useLocation

--- src/Routes/PrivateRoutes.jsx
+++ src/Routes/PrivateRoutes.jsx
@@ -1,9 +1,13 @@
 import React, { use } from 'react';
 import { AuthContext } from '../Context/AuthContext/AuthContext';
-import { Navigate } from 'react-router';
+import { Navigate, useLocation } from 'react-router';
 
 const PrivateRoutes = ({children}) => {
     const {user,loading} = use(AuthContext);
+
+    // useLocation code here;
+    const location = useLocation();
+    console.log(location);
     if(loading){
        return <p>Loadding........</p>
 
